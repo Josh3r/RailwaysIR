e4320dbf4ef0c9ac55ff18190cc37741ae408085
Test <teo.josh@yahoo.com.sg>
2019-11-16 21:19:06 +0800

    Updates

---
+++ precache-manifest.d182645dae23bf9364693de5c1497249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b20f42632f0c9ca49386a89b6a4170a",
+    "url": "/railwaysir/index.html"
+  },
+  {
+    "revision": "76e80ea3427c04a1fa9d",
+    "url": "/railwaysir/static/css/main.1e9a7712.chunk.css"
+  },
+  {
+    "revision": "d12d69671c08c197b243",
+    "url": "/railwaysir/static/js/2.b5bf4a83.chunk.js"
+  },
+  {
+    "revision": "76e80ea3427c04a1fa9d",
+    "url": "/railwaysir/static/js/main.6c6ffad7.chunk.js"
+  },
+  {
+    "revision": "8e088d0e4a8cca05f8e6",
+    "url": "/railwaysir/static/js/runtime-main.362e5e0a.js"
+  }
+]);
